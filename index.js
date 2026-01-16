906be07ba572583e4f383935800d92cbbeb3174f
Mobiiil <phodeu.mm@gmail.com>
2026-01-16 03:17:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log('INICIANDO O BOT...');
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`🤖 Bot ligado como ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('Pong 🏓');
+  }
+});
+
+client.login(process.env.TOKEN);
+  .then(() => console.log('LOGIN ENVIADO AO DISCORD'))
+  .catch(err => console.error('ERRO AO LOGAR:', err));
